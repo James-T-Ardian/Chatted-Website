059fb4c7c69fd5307557f8e94bc05d2e7dad8c4f
James T Ardian <jamesardian01@gmail.com>
2022-02-26 22:14:49 -0800

    added more tests to Room model

--- server/src/model/Rooms.spec.ts
+++ server/src/model/Rooms.spec.ts
@@ -2,28 +2,51 @@ import { MySQLQueryResult } from '../types/types'
 import {Rooms} from './Rooms'
 
 describe('Rooms class', ()=>{
+
     const roomsModel = new Rooms()
-    let testRoom:string
+    let testRoomId: string
 
     test("createNewRoom function", async ()=>{
         // There is a dedicated user for testing Rooms named "RoomsTest" with user_id 14
-        roomsModel.createNewRoom("14")
-        .then((result:MySQLQueryResult)=>{
-            testRoom = result.insertId?.toString() as string
+        return roomsModel.createNewRoom("14")
+        .then((result: MySQLQueryResult)=>{
+            let insertId = result.insertId as number
+            testRoomId = insertId.toString()
             expect(result.affectedRows).toBe(1)
         })
     })
 
-    test("getCreatorIdFromRoomId", async()=>{
-        roomsModel.getCreatorIdFromRoomId(testRoom)
-        .then((result:MySQLQueryResult)=>{
-            expect(result).toEqual([{creator_id: "14"}])
+    test("getCreatorIdFromRoomId", async ()=>{
+        return roomsModel.getCreatorIdFromRoomId(testRoomId)
+        .then((result: MySQLQueryResult)=>{
+            expect(result).toEqual([{creator_id: 14}])
+        })
+    })
+
+    test("addMemberToRoom", async () =>{
+        return roomsModel.addMemberToRoom("14", testRoomId)
+        .then((result: MySQLQueryResult)=>{
+            expect(result.affectedRows).toBe(1)
+        })
+    })
+
+    test("getAllRoomIdsFromMemberId", async ()=>{
+        return roomsModel.getAllRoomIdsFromMemberId("14")
+        .then((result: MySQLQueryResult)=>{
+            expect(result).toEqual([{room_id: Number(testRoomId)}])
+        })
+    })
+
+    test("deleteAllMembersFromRoomId", async ()=>{
+        return roomsModel.deleteAllMembersFromRoomId(testRoomId)
+        .then((result: MySQLQueryResult)=>{
+            expect(result.affectedRows).toBe(1)
         })
     })
 
     test("deleteRoomFromRoomId", async()=>{
-        roomsModel.deleteRoomFromRoomId(testRoom)
-        .then((result:MySQLQueryResult)=>{
+        return roomsModel.deleteRoomFromRoomId(testRoomId)
+        .then((result: MySQLQueryResult)=>{
             expect(result.affectedRows).toBe(1)
         })
     })
--- server/src/model/Users.spec.ts
+++ server/src/model/Users.spec.ts
@@ -2,24 +2,25 @@ import { MySQLQueryResult } from '../types/types'
 import {Users} from './Users'
 
 describe("Users class", ()=>{
+
     const usersModel:Users = new Users()
 
     test("createNewUser function", async ()=>{
-        usersModel.createNewUser("test", "pass")
+        return usersModel.createNewUser("test", "pass")
         .then((result: MySQLQueryResult)=>{
             expect(result.affectedRows).toBe(1)
         })
     })
 
     test("getPasswordFromUsername function", async ()=>{
-        usersModel.getPasswordFromUsername("test")
+        return usersModel.getPasswordFromUsername("test")
         .then((result: MySQLQueryResult)=>{
             expect(result).toEqual([{password: "pass"}])
         })
     })
 
     test("deleteUserFromUsername function", async ()=>{
-        usersModel._deleteUserFromUsername("test")
+        return usersModel._deleteUserFromUsername("test")
         .then((result: MySQLQueryResult)=>{
             expect(result.affectedRows).toBe(1)
         })
